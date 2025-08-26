7b1d7b1036cadb53538c371787164d0c0788c7a6
EuNetu <euneto2501@gmail.com>
2025-08-26 15:48:41 -0300

    chore: reconfigura eslint e as libs do projeto

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,23 +1,39 @@
-import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import hooksPlugin from 'eslint-plugin-react-hooks'
+import refreshPlugin from 'eslint-plugin-react-refresh'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  {
+    ignores: ['dist/**'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'react-hooks': hooksPlugin,
+      'react-refresh': refreshPlugin,
+    },
+    rules: {
+      ...pluginReactConfig.rules,
+      ...hooksPlugin.configs.recommended.rules,
+      'react-refresh/only-export-components': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-])
+]
