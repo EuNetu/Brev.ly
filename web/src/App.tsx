ee444ebf6ba187bd8e9ca398e0f121da0753585f
EuNetu <euneto2501@gmail.com>
2025-08-25 22:23:24 -0300

    chore: cria estrutura inicial do projeto

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="min-h-screen bg-gray-200 flex items-center justify-center">
+      <h1 className="text-blue-base text-3xl font-bold">
+        Olá, Brev.ly!
+      </h1>
+    </div>
+  )
+}
